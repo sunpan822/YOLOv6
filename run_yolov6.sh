db8cf7fe99d73a34c3b999a322f9fcfb99ded79d
sunpan822 <sunpan822@163.com>
2022-11-19 15:49:34 +0800

    Update run_yolov6.sh

--- run_yolov6.sh
+++ run_yolov6.sh
@@ -5,6 +5,6 @@ cd /project/train/src_repo/YOLOv6
 
 
 # cp /project/train/models/exp/weights/last_ckpt.pt /project/train/models/exp/weights/best_ckpt.pt
-python /project/train/src_repo/YOLOv6/tools/train.py --batch 32 --epochs 19 --data /project/train/src_repo/YOLOv6/data/objects.yaml --conf /project/train/src_repo/YOLOv6/configs/yolov6s_finetune.py  --img-size 640 --output-dir /project/train/models/ --workers 4 --resume /project/train/models/exp/weights/last_ckpt.pt
+python /project/train/src_repo/YOLOv6/tools/train.py --batch 32 --epochs 19 --data /project/train/src_repo/YOLOv6/data/custom.yaml --conf /project/train/src_repo/YOLOv6/configs/yolov6s_finetune.py  --img-size 640 --output-dir /project/train/models/ --workers 4 --resume /project/train/models/exp/weights/last_ckpt.pt
 
-# python /project/train/src_repo/YOLOv6/tools/train.py --batch 32 --epochs 100 --data /project/train/src_repo/YOLOv6/data/objects.yaml --conf /project/train/src_repo/YOLOv6/configs/yolov6s_finetune.py  --img-size 640 --output-dir /project/train/models/ --workers 4 
+# python /project/train/src_repo/YOLOv6/tools/train.py --batch 32 --epochs 100 --data /project/train/src_repo/YOLOv6/data/custom.yaml --conf /project/train/src_repo/YOLOv6/configs/yolov6s_finetune.py  --img-size 640 --output-dir /project/train/models/ --workers 4 
